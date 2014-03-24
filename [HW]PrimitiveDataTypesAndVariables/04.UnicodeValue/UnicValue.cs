d1e20b98328de2603c35b6d0b21c6d69b47cffb3
Heru_EldA <yulia.ten@dezigner.ru>
2014-03-24 23:48:38 +0200

    Must find the hex of 42, not 72 (by the new version of HW-tasks document)

--- [HW]PrimitiveDataTypesAndVariables/04.UnicodeValue/UnicValue.cs
+++ [HW]PrimitiveDataTypesAndVariables/04.UnicodeValue/UnicValue.cs
@@ -1,8 +1,9 @@
-﻿// Problem 4. Unicode Value
+﻿// Problem 4. Unicode Character
 // Declare a character variable and assign it with the symbol that has 
-// Unicode code 72, and then print it. Hint: first, use the Windows 
-// Calculator to find the hexadecimal representation of 72. 
-// The output should be “H”.
+// Unicode code 42 (decimal) using the '\u00XX' syntax, and then print it. 
+// Hint: first, use the Windows Calculator to find the hexadecimal 
+// representation of 42. The output should be “*”.
+
 
 using System;
 using System.Text;
@@ -14,7 +15,7 @@ class UnicValue
         Console.Title = "Problem 4. Unicode Value";
         Console.OutputEncoding = Encoding.GetEncoding("UTF-8");
 
-        char symbol = '\u0048'; // The hex of 72 is 48.
+        char symbol = '\u002A'; // The hex of 42 is 2A.
         Console.WriteLine(symbol);
 
         //Just a check for cyrillic symbols
