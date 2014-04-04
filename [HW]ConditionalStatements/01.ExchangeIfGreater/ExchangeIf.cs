add2f909ef95ab4f6888167b37728d161c291760
y-lyn <yulia.ten@dezigner.ru>
2014-04-04 18:46:13 +0300

    Uploaded

---
+++ [HW]ConditionalStatements/01.ExchangeIfGreater/ExchangeIf.cs
@@ -0,0 +1,46 @@
+﻿// Problem 1. Exchange If Greater
+// Write an if-statement that takes two integer variables a and b 
+// and exchanges their values if the first one is greater than the 
+// second one. As a result print the values a and b, separated by 
+// a space. Example: a = 5, b = 2 --> Result: a = 2, b = 5;
+
+using System;
+
+class ExchangeIf
+{
+    static void Main()
+    {
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(new string('-',6));
+
+        if (a == b)
+        {
+            Console.WriteLine("Equals");
+        }
+        else if (a > b)
+        {
+            Console.WriteLine("a = {0}\nb = {1}\n", b, a);
+        }
+        else
+        {
+            Console.WriteLine("a = {0}\nb = {1}\n", a, b);
+        }
+        
+        //All this could be solved with the operator ?:
+        //Console.Write("a = {0}\nb = {1}\n", (a > b) ? b : a, (a > b) ? a : b);
+
+       
+
+
+
+        
+
+
+    
+    }
+}
+
---
+++ [HW]ConditionalStatements/06. BiggestOfFive/BiggestOfFive.cs
@@ -0,0 +1,29 @@
+﻿// Problem 6. The Biggest of Five Numbers
+// Write a program that finds the biggest of 
+// five numbers by using only five if statements. 
+
+using System;
+
+class BiggestOfFive
+{
+    static void Main()
+    {
+        Console.WriteLine("Please, insert 5 numbers: ");
+        double num1 = double.Parse(Console.In.ReadLine());
+        double num2 = double.Parse(Console.In.ReadLine());
+        double num3 = double.Parse(Console.In.ReadLine());
+        double num4 = double.Parse(Console.In.ReadLine());
+        double num5 = double.Parse(Console.In.ReadLine());
+
+        Console.Write("\nThe greatest number of them is: ");
+
+        //Use five If-statements? Okay...
+        if (num1 >= num1 && num1 >= num2 && num1 >= num3 && num1 >= num4 && num1 >= num5) Console.Write(num1);
+        if (num2 >= num1 && num2 >= num2 && num2 >= num3 && num2 >= num4 && num2 >= num5) Console.Write(num2);
+        if (num3 >= num1 && num3 >= num2 && num3 >= num3 && num3 >= num4 && num3 >= num5) Console.Write(num3);
+        if (num4 >= num1 && num4 >= num2 && num4 >= num3 && num4 >= num4 && num4 >= num5) Console.Write(num4);
+        if (num5 >= num1 && num5 >= num2 && num5 >= num3 && num5 >= num4 && num5 >= num5) Console.Write(num5);
+
+        Console.WriteLine();
+    }
+}
