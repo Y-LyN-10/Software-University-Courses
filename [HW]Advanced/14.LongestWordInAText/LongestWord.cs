c6cd797aa8e4eddbdd01ef9d23ca1e4a43a4c568
y-lyn <yulia.ten@dezigner.ru>
2014-04-13 21:55:26 +0300

    Ready

--- [HW]Advanced/14.LongestWordInAText/LongestWord.cs
+++ [HW]Advanced/14.LongestWordInAText/LongestWord.cs
@@ -4,12 +4,40 @@
 //           Output - University
 
 using System;
+using System.IO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        //Test 1:
+        //Console.SetIn(new StreamReader("../../input.txt"));
+        //Test 2:
+        //Console.SetIn(new StreamReader("../../input2.txt"));
 
+        string input = Console.ReadLine().TrimEnd('.');
+        string[] words = input.Split(' ');
+
+        int letterCounter = 0;
+        int maxCount = 0;
+        string longestWord = "";
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words[i].Length; j++)
+            {
+                letterCounter++;
+                
+                if (letterCounter >= maxCount)
+                {
+                    maxCount = letterCounter;
+                    longestWord = words[i];
+                }
+            }
+            letterCounter = 0;
+        }
+        Console.WriteLine(longestWord);
     }
 }
 
