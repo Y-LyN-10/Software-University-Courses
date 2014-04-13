573bb72fc7e795b211430c728c512a32f69b216e
y-lyn <yulia.ten@dezigner.ru>
2014-04-13 21:21:34 +0300

    Ready

--- [HW]Advanced/10.JoinLists/JoinLists.cs
+++ [HW]Advanced/10.JoinLists/JoinLists.cs
@@ -8,11 +8,29 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        //Test 1:
+        Console.SetIn(new StreamReader("../../input.txt"));
+        //Test 2:
+        //Console.SetIn(new StreamReader("../../input2.txt"));
+        //Test 3:
+        //Console.SetIn(new StreamReader("../../input3.txt"));
 
+        //After long hours of solving this problem, I decided to use SortedSet class
+        //It sorts automatically unique elements in increasing order.
+
+        string[] firstLine =  Console.ReadLine().Split(' ');
+        string[] secondLine = Console.ReadLine().Split(' ');
+
+        SortedSet<int> sortedNumbers = new SortedSet<int>();
+        foreach (var number in firstLine) sortedNumbers.Add(int.Parse(number));
+        foreach (var number in secondLine) sortedNumbers.Add(int.Parse(number));
+        foreach (var number in sortedNumbers) Console.Write(number + " ");
     }
 }
