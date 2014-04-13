750b1fa6d0bfe0127528acf84bfaa14e4b9580aa
y-lyn <yulia.ten@dezigner.ru>
2014-04-13 16:57:48 +0300

    Ready

--- [HW]Advanced/09.RemoveNames/RemoveNames.cs
+++ [HW]Advanced/09.RemoveNames/RemoveNames.cs
@@ -6,12 +6,42 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class RemoveNames
 {
     static void Main()
     {
+        //Test 1:
+        //Console.SetIn(new StreamReader("../../input.txt"));
+        //Test 2:
+        //Console.SetIn(new StreamReader("../../input2.txt"));
 
+        string[] firstLine = Console.ReadLine().Split(' ');
+        List<string> names = new List<string>();
+
+        string[] secondLine = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < firstLine.Length; i++)
+        {
+            names.Add(firstLine[i]); //fill the list
+
+            for (int j = 0; j < secondLine.Length; j++)
+            {
+                if (firstLine[i] == secondLine[j])
+                {
+                    names.Remove(firstLine[i]); //remove repeats
+                }
+            }
+        }
+
+        //Print the result
+        foreach (var name in names)
+        {
+            Console.Write(name + " ");
+        }
+        Console.WriteLine();
     }
 }
 
--- [HW]Advanced/11.CountOfLetters/CountOfLetters.cs
+++ [HW]Advanced/11.CountOfLetters/CountOfLetters.cs
@@ -8,12 +8,46 @@
 //                          b -> 3
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        Console.WriteLine("Test 1:");
+        Console.SetIn(new StreamReader("../../input.txt"));
+        Solution();
 
+        Console.WriteLine("Test 2:");
+        Console.SetIn(new StreamReader("../../input2.txt"));
+        Solution();
+    }
+    
+    static void Solution()
+    {
+        string[] letters = Console.ReadLine().Split(' ');
+        Array.Sort(letters);
+
+        var alphabet = new HashSet<string>();
+        for (int i = 0; i < letters.Length; i++)
+        {
+            alphabet.Add(letters[i]);
+        }
+
+        int count = 0;
+        foreach(string letter in alphabet)
+        {
+            for (int j = 0; j < letters.Length; j++)
+            {
+                if (letter == letters[j]) count++;
+            }
+            
+            Console.WriteLine(letter + " -> " + count);
+            count = 0;
+        }
+        Console.WriteLine();
     }
 }
 
