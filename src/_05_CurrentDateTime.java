94468b59a0873988d93cc0a75a3ec9c196b24538
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-05-15 19:57:31 +0300

    Upload solutions

---
+++ src/_05_CurrentDateTime.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class _05_CurrentDateTime {
+
+	public static void main(String[] args) {
+		
+		LocalDateTime todayDate = LocalDateTime.now();
+		System.out.println(todayDate);
+	}
+}
