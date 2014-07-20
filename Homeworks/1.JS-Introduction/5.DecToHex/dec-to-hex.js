f65e00844454faeaf0d6d98ce2515b42d4be0088
Y-Lyn-10 <yulia.ten@dezigner.ru>
2014-07-20 10:36:42 +0300

    Solved a problem from HW-1

--- Homeworks/1.JS-Introduction/5.DecToHex/dec-to-hex.js
+++ Homeworks/1.JS-Introduction/5.DecToHex/dec-to-hex.js
@@ -3,44 +3,17 @@
  num and converts and converts it to a hexadecimal form. The input should be entered as
  JavaScript prompt window. The output should be shown as browser popup window (alert).  */
 
-// Only console working for now.
+'use strict';
+
 function convertDecToHex(decNumber) {
-    var answer = '';
-
-    if (decNumber < 0) {
-        return false;
-        //TODO: Throw an exception
-    } else {
-
-        while (decNumber != 0) {
-            if (decNumber % 16 > 9) {
-                switch (decNumber % 16) {
-                    case 10:
-                        answer = "A" + answer;
-                        break;
-                    case 11:
-                        answer = "B" + answer;
-                        break;
-                    case 12:
-                        answer = "C" + answer;
-                        break;
-                    case 13:
-                        answer = "D" + answer;
-                        break;
-                    case 14:
-                        answer = "E" + answer;
-                        break;
-                    case 15:
-                        answer = "F" + answer;
-                        break;
-                }
-            } else {
-                answer = decNumber % 16 + answer;
-            }
-            decNumber = decNumber / 16;
-        }
-        return answer;
-    }
+    var hexNumber = Number(decNumber).toString(16).toUpperCase();
+    return hexNumber;
 }
 
-convertDecToHex(254);
+/* Examples, given in the task:
+   Input:       Output:
+   254	        FE
+   6779	        1A7B
+   10941298 	A6F372
+   65535	    FFFF
+   0	        0           */
