814e79c6542078d50dbf364bb41be49f92758bbe
Y-Lyn-10 <yulia.ten@dezigner.ru>
2014-08-05 23:45:24 +0300

    JS Homework Uploaded

---
+++ Homeworks/4.Functions-And-Objects/05.Reverse-Words/reverse-words.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+
+    function reverseWords(string) {
+        var words, reversedWord,
+            output = [], i;
+
+        words = string.split(' ')
+            .filter(Boolean)
+            .forEach(function (word) {
+                reversedWord = [];
+                for (i = 0; i < word.length; i += 1) {
+                    reversedWord[i] = word[word.length - 1 - i];
+                }
+                output.push(reversedWord.join(''));
+            });
+
+        return output.join(' ');
+    }
+
+    var testStrings = [
+        'Hello, how are you.',
+        'Life is pretty good, isn’t it?',
+        'I really love JavaScript!',
+        'Look at this amazing library - chance!',
+        chance.sentence({words: 6}),
+        chance.paragraph({sentences: 2})
+    ],
+        reversed, result;
+
+    testStrings.forEach(function(string){
+        reversed = reverseWords(string);
+
+        result = {
+            string: string,
+            reversed: reversed
+        };
+        console.log(result);
+    });
+}());
---
+++ Homeworks/4.Functions-And-Objects/06.Count-DIVs/count-divs.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    var divs = document.querySelectorAll('div');
+    console.log(divs.length);
+
+}());
