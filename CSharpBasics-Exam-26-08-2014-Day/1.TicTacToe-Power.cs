3c3d3e27ad7d7ec08098dd5e07a8f4b301cedb9e
Y-Lyn-10 <yulia.ten@dezigner.ru>
2014-08-27 23:30:35 +0200

    Exam Solutions in CSharp Uploaded

---
+++ CSharpBasics-Exam-26-08-2014-Day/1.TicTacToe-Power.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+class TicTacToePower
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int v = int.Parse(Console.ReadLine());
+
+        int nextValue = v;
+
+        int[,] ticTacToe = new int[3, 3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                ticTacToe[i, j] = nextValue;
+                if(i == y && j == x){
+                    Console.WriteLine((BigInteger)(Math.Pow(nextValue, (nextValue + 1) - v)));
+                }
+                nextValue ++;
+            }
+        }
+    }
+}
