6a465e7074ae1be2c65ca8f37f373e52433f27a0
Y-Lyn-10 <yulia.ten@dezigner.ru>
2014-09-14 01:37:00 +0300

    Ready to merge with Software-University-Courses Repo

---
+++ CSharp-SoftUni/[HW]PrimitiveDataTypesAndVariables/06.StringsAndObjects/StringsAndObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class StringsAndObjects
+{
+    static void Main()
+    {
+        Console.Title = "Problem 6. Strings and Objects";
+
+        // Declare two string variables and assign them with “Hello” and “World”. 
+        string hello = "Hello";
+        string world = "World";
+
+        // Declare an object variable and assign it with the concatenation of the 
+        // first two variables (mind adding an interval between). 
+        object helloWorld = hello + " " + world;
+
+        // Declare a third string variable and initialize it with the value of 
+        // the object variable (you should perform type casting).
+        string printHelloWorld = helloWorld.ToString();
+
+        //Print the final string:
+        Console.WriteLine(printHelloWorld);
+    }
+}
+
