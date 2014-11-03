761d188acf548d78c868dab58572e14e8e38fece
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-11-03 20:02:05 +0200

    forms added, styling and etc

--- JavaScript-OOP/2-Object-Oriented-Programming/Canvas-Geometry-API/scripts/shapes.js
+++ JavaScript-OOP/2-Object-Oriented-Programming/Canvas-Geometry-API/scripts/shapes.js
@@ -1,37 +1,38 @@
 var shapeModule = (function () {
     'use strict';
 
-    function Shape(x, y, color) {
+    function Shape(x, y, strokeColor, fillColor) {
         this._x = x;
         this._y = y;
-        this._color = color;
+        this._strokeColor = strokeColor;
+        this._fillColor = fillColor;
     }
 
-    function Point(x, y, color) {
-        Shape.call(this, x, y, color);
+    function Point(x, y, strokeColor, fillColor) {
+        Shape.call(this, x, y, strokeColor, fillColor);
     }
 
-    function Circle(x, y, color, r) {
-        Shape.call(this, x, y, color);
-        this._r = r;
+    function Circle(x, y, strokeColor, fillColor, radius) {
+        Shape.call(this, x, y, strokeColor, fillColor);
+        this._radius = radius;
     }
 
-    function Rectangle(x, y, color, width, height) {
-        Shape.call(this, x, y, color);
+    function Rectangle(x, y, strokeColor, fillColor, width, height) {
+        Shape.call(this, x, y, strokeColor, fillColor);
         this._width = width;
         this._height = height;
     }
 
-    function Triangle(x, y, color, x2, y2, x3, y3) {
-        Shape.call(this, x, y, color);
+    function Triangle(x, y, strokeColor, fillColor, x2, y2, x3, y3) {
+        Shape.call(this, x, y, strokeColor, fillColor);
         this._x2 = x2;
         this._y2 = y2;
         this._x3 = x3;
         this._y3 = y3;
     }
 
-    function Segment(x, y, color, x2, y2) {
-        Shape.call(this, x, y, color);
+    function Segment(x, y, strokeColor, fillColor, x2, y2) {
+        Shape.call(this, x, y, strokeColor, fillColor);
         this._x2 = x2;
         this._y2 = y2;
     }
