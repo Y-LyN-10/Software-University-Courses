db683a7a5b1f406cd0f0487784e218a0cee3025e
Yulia Tenincheva <ylin@edigresyl.xs-software.com>
2015-01-06 18:20:04 +0200

    Pagination

--- SPA-AngularJS/Lab/app/js/controllers/HomeController.js
+++ SPA-AngularJS/Lab/app/js/controllers/HomeController.js
@@ -1,8 +1,15 @@
 'use strict';
 
-// The HomeController holds the presentation logic for the home screen
 app.controller('HomeController',
-    function ($scope, $rootScope, adsService, notifyService, pageSize) {
-		// TODO
-	}
+    function ($scope, adsService, notifyService) {
+        adsService.getAds(
+            null,
+            function success(data) {
+                $scope.ads = data;
+            },
+            function error(err) {
+                notifyService.showError("Cannot load ads", err);
+            }
+        );
+    }
 );
--- SPA-AngularJS/Lab/app/js/services/adsServices.js
+++ SPA-AngularJS/Lab/app/js/services/adsServices.js
@@ -1,12 +1,23 @@
 'use strict';
 
 app.factory('adsService',
-	function ($resource, baseServiceUrl) {
-		return {
-			// TODO: implement a service to get ads
-		};
+    function ($resource, baseServiceUrl) {
+        var adsResource = $resource(
+            baseServiceUrl + '/api/ads',
+            null,
+            {
+                'getAll': {method:'GET'}
+            }
+        );
+
+        return {
+            getAds: function(params, success, error) {
+                return adsResource.getAll(params, success, error);
+            }
+        }
     }
 );
+
 
 app.factory('townsService',
 	function ($resource, baseServiceUrl) {
